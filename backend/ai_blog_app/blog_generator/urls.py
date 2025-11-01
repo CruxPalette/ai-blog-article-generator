1dc4b57dfe47ad1efc94cd2b32eb86ea0d246456
CruxPalette <zhaochen020410@gmail.com>
2025-11-01 18:47:43 -0400

    JS with Ajax

--- backend/ai_blog_app/blog_generator/urls.py
+++ backend/ai_blog_app/blog_generator/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.user_login, name='login'),
     path('signup', views.user_signup, name='signup'),
-    path('logout', views.user_logout, name='logout')
-    path('generate-blog', vies.generate_blog, name='generate-blog')
+    path('logout', views.user_logout, name='logout'),
+    path('generate-blog', views.generate_blog, name='generate-blog')
 
 ]
